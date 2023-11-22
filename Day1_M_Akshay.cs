574d390b3f0d1f1ba4fe0b1b40f7bdf1549d409f
AkshayKunjilwar <144681625+AkshayKunjilwar@users.noreply.github.com>
2023-11-22 11:17:15 +0530

    Create Day1_M_Akshay.cs

---
+++ Day1_M_Akshay.cs
@@ -0,0 +1,80 @@
+//Review1 - int d;
+int ElapsedTimeInDays;
+
+//Review2-
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees 
+var ListOfEmployees =  db.GetFromService().ToList();
+
+//Review3 -
+int iCounter;
+            string strFullName;
+            DateTime dModifiedDate;
+int Counter;
+string FullName;
+DateTime ModifiedDate;
+
+//Review-4
+public bool IsShopOpen(string pDay, int pAmount)
+{
+    // some logic
+}
+
+public bool IsShopOpen(string Day, int Amount)
+{
+    // some logic
+}
+
+//Review5-
+const int DAYS_IN_WEEK = 7;
+const int daysInMonth = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool Restore_database() {}
+
+class animal {}
+class Alpaca {}
+
+const int DaysInWeek = 7;
+const int DaysInMonth = 30;
+
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+
+bool EraseDatabase() {}
+bool RestoreDatabase() {}
+
+class Animal {}
+class Alpaca {}
+
+//Review6-
+public class Employee
+{
+    public Datetime sWorkDate { get; set; } // get set Start Working Date
+    public Datetime modTime { get; set; } // get set Modification Time
+}
+
+public class Employee
+{
+    public Datetime StartWorkDate { get; set; } // get set Start Working Date
+    public Datetime ModificationTime { get; set; } // get set Modification Time
+}
+
+//Review7 - 
+var employeephone;
+
+public double CalculateSalary(int workingdays, int workinghours)
+{
+    // some logic
+}
+
+var Employeephone;
+
+public double CalculateSalary(int Workingdays, int Workinghours)
+{
+    // some logic
+}
+
+
